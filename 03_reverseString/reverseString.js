56fb61fa9bc24e3409c8133cce086484407553df
Nathan Ma <jiama19999@gmail.com>
2024-01-12 16:29:41 -0600

    Exercise 3 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let ret = "";
+    for (let i=inputString.length-1; i>=0; i--) {
+        ret += inputString.charAt(i);
+    }
+    return ret;
 };
 
 // Do not edit below this line
