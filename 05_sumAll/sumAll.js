9630d95094f5d40a6708bbcf86972022c4951875
Nathan Ma <jiama19999@gmail.com>
2024-01-12 16:58:13 -0600

    Exercise 5 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (typeof a != "number" || typeof b != "number") {
+        return "ERROR";
+    }
+    if (a<0 || b<0) {
+        return "ERROR";
+    }
+    if (a<b) {
+        return (b-a+1)*(a+b)/2;
+    }
+    return (a-b+1)*(a+b)/2;
 };
 
 // Do not edit below this line
