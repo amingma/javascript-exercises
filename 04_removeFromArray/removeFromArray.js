ec15ccdfeb8c491776bc3aeabb50b6377f26afae
Nathan Ma <jiama19999@gmail.com>
2024-01-12 16:52:21 -0600

    Exercise 4 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
 const removeFromArray = function() {
-
+    let arr = arguments[0];
+    let ret = [];
+    for (let i=0; i<arr.length; i++) {
+        let add = true;
+        for (let j=1; j<arguments.length; j++) {
+            if (arr[i]===arguments[j]) {
+                add = false;
+            }
+        }
+        if (add) {
+            ret.push(arr[i]);
+        }
+    }
+    return ret;
 };
 
 // Do not edit below this line
