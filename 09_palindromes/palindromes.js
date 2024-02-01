68b8155e8a29069187c9b0a806a9164b1b8e6e8f
Nathan Ma <jiama19999@gmail.com>
2024-01-31 19:08:58 -0600

    Exercise 9 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
 const palindromes = function () {
-
+    var input = arguments[0];
+    input = input.toLowerCase();
+    const alpha = [];
+    const alphabet = "abcdefghijklmnopqrsrtuvwxyz0123456789";
+    for (var i=0; i<36; i++) {
+        alpha.push(alphabet.charAt(i));
+    }
+    var newString = "";
+    for (var i=0; i<input.length; i++) {
+        for (var j=0; j<36; j++) {
+            if (input[i]==alpha[j]) {
+                newString += input[i];
+            }
+        }
+    }
+    for (var i=0; i<(newString.length-1)/2; i++) {
+        if (newString.charAt(i)!=newString.charAt(newString.length-1-i)){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
