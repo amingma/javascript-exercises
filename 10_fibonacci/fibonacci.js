6e92e02a56bbd662f0c2e818ce1db1350370df86
Nathan Ma <jiama19999@gmail.com>
2024-01-31 19:19:48 -0600

    Exercise 10 complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
 const fibonacci = function() {
-
+    var num = arguments[0];
+    if (typeof num == "string") {
+        num = parseInt(num);
+    }
+    if (num < 0) {
+        return "OOPS";
+    }
+    fib = [0, 1];
+    for (var i=2; i<=num; i++) {
+        fib.push(fib[i-1]+fib[i-2]);
+    }
+    return fib[num];
 };
 
 // Do not edit below this line
